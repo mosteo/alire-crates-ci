40b09cb190814f69f6fcf002b70249e3f425c309
Alejandro R. Mosteo <amosteo@unizar.es>
2020-04-30 12:56:10 +0200

    Fixes for CI batch crate builds

--- test-script.sh
+++ test-script.sh
@@ -13,6 +13,7 @@ set -o nounset
 git clone --recurse-submodules https://github.com/alire-project/alire
 pushd alire
 commit=`git log --pretty=oneline -1 | cut -c1-8`
+[[ "$OSTYPE" =~ darwin.* ]] && export OS=macOS
 gprbuild -j0 -p -P alr_env
 export PATH+=:`pwd`/bin
 popd
@@ -35,6 +36,27 @@ if [ "`find $testdir -name '*.md' | wc -l`" -gt 0 ]; then
 else
     echo "alr test failed to run in $dst" > $dst.md
 fi
+
+# SYNC BARRIER To avoid conflicts when pushing results, we allow each setup to
+# only do so at a specific minute. The root of this problem is at GH not
+# allowing using "needs:" on matrix jobs.
+
+# Array vars fail on MacOS bash version. Resort to a more verbose case
+case $SETUP_NAME in
+    centos-latest-community-latest) ending=0 ;;
+    community-latest)               ending=1 ;;
+    debian-stable)                  ending=2 ;;
+    ubuntu-lts)                     ending=3 ;;
+    windows-latest)                 ending=4 ;;
+    macos-latest)                   ending=5 ;;
+esac
+
+while ! [[ "$(date +%M)" =~ .$ending ]]; do
+    echo Waiting for slot $ending at $(date)
+    sleep 30s
+done
+
+echo Barrier left behind at $(date)
 
 # push to publishing branch
 remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
